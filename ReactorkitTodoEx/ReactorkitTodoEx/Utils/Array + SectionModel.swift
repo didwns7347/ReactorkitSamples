47c3b6bff40f413f291689888ff2d42dcf3a6fb5
양준수 <yangjs@markany.com>
2023-08-02 16:17:50 +0900

    ReactorkitTodo구현 완료

--- ReactorkitTodoEx/ReactorkitTodoEx/Utils/Array + SectionModel.swift
+++ ReactorkitTodoEx/ReactorkitTodoEx/Utils/Array + SectionModel.swift
@@ -5,4 +5,51 @@
 //  Created by yangjs on 2023/08/02.
 //
 
-import Foundation
+import RxDataSources
+
+
+extension Array where Element: SectionModelType {
+    public subscript(indexPath: IndexPath) -> Element.Item {
+        get {
+            return self[indexPath.section].items[indexPath.row]
+        }
+        mutating set {
+            self.update(section: indexPath.section) { items in
+                items[indexPath.item] = newValue
+            }
+        }
+    }
+    
+    public mutating func insert(
+        _ newItem: Element.Item,
+        at indexPath: IndexPath
+    ) {
+        self.update(section: indexPath.section) { items in
+            items.insert(newItem, at: indexPath.item)
+        }
+    }
+    
+    @discardableResult
+    public mutating func remove(
+        at indexPath: IndexPath
+    ) -> Element.Item {
+        return self.update(section: indexPath.section) { items in
+            items.remove(at: indexPath.row)
+        }
+    }
+    
+    private mutating func replace(section: Int, items: [Element.Item]) {
+        self[section] = Element(original: self[section], items: items)
+    }
+    
+    
+    private mutating func update<T>(
+        section: Int,
+        mutate: (inout [Element.Item]) -> T
+    ) -> T {
+        var items = self[section].items
+        var value = mutate(&items)
+        self[section] = Element.init(original: self[section], items: items)
+        return value
+    }
+}
